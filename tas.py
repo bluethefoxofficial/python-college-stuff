74144088377f6fa501f0325afc14acecaf3c70cf
bluethefoxofficial <47904806+bluethefoxofficial@users.noreply.github.com>
2022-02-17 09:46:27 +0000

    eeeeeeeeeeeee
    eeeeeeeeeeeeeeeeeee

--- tas.py
+++ tas.py
@@ -129,3 +129,5 @@ def quiz():
     print(str(percentage)+"%")
     
 quiz()
+
+
---
+++ tas2.py
@@ -0,0 +1,9 @@
+
+num = 1
+
+
+while num <= 100:
+    if(num % 2 != 0):
+        print("{0}".format(num))
+    num += 1
+        
